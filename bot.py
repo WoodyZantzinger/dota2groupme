39a78ae219ff90931024b0aa66325fee34b49b0d
Woody Zantzinger <wz2ae@virginia.edu>
2015-01-15 20:42:33 -0500

    new post

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+#
+# https://api.groupme.com/v3/bots/post
+
+@app.route('/message/', methods=['POST'])
+def message():
+	text=request.form['text']
+	print "New Message: " + text
+	return text
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+	return "Hello world!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
