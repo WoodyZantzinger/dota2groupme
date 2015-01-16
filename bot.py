6d33a867486c3264bada0ea2f2a1f187f023c9e9
Woody Zantzinger <wz2ae@virginia.edu>
2015-01-16 09:52:31 -0500

    reads commands

--- bot.py
+++ bot.py
@@ -1,15 +1,46 @@
 import os
+import urllib2
+import urllib
 from flask import Flask, request
 
 app = Flask(__name__)
 #
 # https://api.groupme.com/v3/bots/post
 
+def send_message(msg):
+	print "Sending " + msg
+	url = 'https://api.groupme.com/v3/bots/post'
+	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+	header = { 'User-Agent' : user_agent }
+	values = {
+      'bot_id' : '535cbd947cf38b46a83fa3084f',
+      'text' : msg,
+	}
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data, header)
+	response = urllib2.urlopen(req)
+	return response
+	
+
+def last_game():
+	return
+
+def current_online():
+	return send_message("No one is online!")
+
+options = {"#last" : last_game,
+           "#now" : current_online,
+}
+
 @app.route('/message/', methods=['POST'])
 def message():
-	text=request.form['text']
-	print "New Message: " + text
-	return text
+	new_message = request.get_json(force=True)
+	sender = new_message["name"]
+	body = new_message["text"]
+	if body.startswith("#"):
+		options[body]()
+	
+	return 
 
 @app.route("/")
 def hello():
