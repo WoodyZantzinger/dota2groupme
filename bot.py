aa068bd19ac17494da2813ac706efc9384a2c989
Woody Zantzinger <wz2ae@virginia.edu>
2015-01-16 18:27:16 -0500

    Added better last and random team

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from flask import Flask, request
 from dota2py import api
 from dota2py import data
 
+DEBUG = False
 
 GroupMetoSteam = {
       'Woody Zantzinger' : 76561197990341684,
@@ -39,27 +40,46 @@ help_dict = {"#last" : "Shows your personel stats from the last game, add a user
            "#help" : "You are reading the help now...",
            "#next" : "Picks a random hero to play...",
            "#nextItem" : "Picks a random item to buy...",
+           "#nextTeam" : "Picks a awesome team to play...",
 }
 
 app = Flask(__name__)
 #
 # https://api.groupme.com/v3/bots/post
 
+def random_hero():
+	next_hero = data.get_hero_name(random.randint(1, 107))
+	if next_hero is not None:
+		return str(next_hero["localized_name"])
+	else:
+		return random_hero()
+
+		
+def random_item():
+	next_item = data.get_item_name(random.randint(1, 212))
+	if next_item is not None:
+		return str(next_item["name"])
+	else:
+		return next_item()
+
+
 def send_message(msg):
 	print "Sending " + msg
-	url = 'https://api.groupme.com/v3/bots/post'
-	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-	header = { 'User-Agent' : user_agent }
-	values = {
-      'bot_id' : '535cbd947cf38b46a83fa3084f',
-      'text' : msg,
-	}
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data, header)
-	response = urllib2.urlopen(req)
-	#print "msg"
-	return response
-	#return 'Win'
+	if not DEBUG:
+		url = 'https://api.groupme.com/v3/bots/post'
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+		header = { 'User-Agent' : user_agent }
+		values = {
+    	  'bot_id' : '535cbd947cf38b46a83fa3084f',
+    	  'text' : msg,
+		}
+		data = urllib.urlencode(values)
+		req = urllib2.Request(url, data, header)
+		response = urllib2.urlopen(req)
+		#print "msg"
+		return response
+	else:
+		return 'Win'
 	
 def set_steam(msg, user):
 	print "Got here"
@@ -96,10 +116,27 @@ def last_game(msg, user):
 	#Get the full details for a match
 	match = api.get_match_details(matches[0]["match_id"])
 	print "Got Match Details"
+	player_num = 0
 	for x in match["result"]["players"]:
 		if int(x["account_id"]) == GroupMetoDOTA[user]:
 			print "Got User Data"
-			send_message ("As " + data.get_hero_name(x["hero_id"])["localized_name"] + " you went " + str(x["kills"]) + ":" + str(x["deaths"]) + ":" + str(x["assists"]) + " with " + str(x["gold_per_min"]) + " GPM")
+			
+			#Stats?
+			send_message("As " + data.get_hero_name(x["hero_id"])["localized_name"] + " you went " + str(x["kills"]) + ":" + str(x["deaths"]) + ":" + str(x["assists"]) + " with " + str(x["gold_per_min"]) + " GPM finishing at level " + str(x["level"]))
+			
+			#Items?
+			finalItems = "Your items: "
+			for itemNum in range(0, 6):
+				if x["item_" + str(itemNum)] != 0:
+					finalItems += str(data.get_item_name(x["item_" + str(itemNum)])["name"]) + ", "
+			send_message(finalItems)
+			
+			#Win?
+			if player_num < 5 and match["result"]["radiant_win"]:
+				send_message("You Won!")
+			else:
+				send_message("You Lost.... Bitch")
+		player_num = player_num +1	
 	return 'OK'
 
 def current_online(msg, user):
@@ -115,19 +152,13 @@ def status(msg, user):
 	return send_message("Currently listening...")
 
 def next(msg, user):
-	next_hero = data.get_hero_name(random.randint(1, 107))
-	if next_hero is not None:
-		return send_message("You will play " + str(next_hero["localized_name"]))
-	else:
-		return next(msg, user)
+	return send_message("You will play " + random_hero())
 	
 def nextItem(msg, user):
-	next_item = data.get_item_name(random.randint(1, 212))
-	if next_item is not None:
-		return send_message("You will buy " + str(next_item["name"]))
-	else:
-		return next_item(msg, user)
-		
+	return send_message("You will buy " + random_item())
+	
+def nextTeam(msg, user):
+	return send_message("The best team ever: " + random_hero() + ", " + random_hero() + ", " + random_hero() + ", " + random_hero() + ", " + random_hero())	
 
 
 options = {"#last" : last_game,
@@ -138,6 +169,7 @@ options = {"#last" : last_game,
            "#help" : help,
            "#next" : next,
            "#nextItem" : nextItem,
+           "#nextTeam" : nextTeam,
 }
 
 
@@ -163,5 +195,7 @@ def hello():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    #app.run(host='0.0.0.0', port=port, debug=True)
+    if DEBUG:
+    	app.run(host='0.0.0.0', port=port)
+    else:
+    	app.run(host='0.0.0.0', port=port, debug=True)
