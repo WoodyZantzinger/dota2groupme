aab8e3dbddc3c2d61d625abc899f1efc7739bd75
Michael Desrosiers <mjd2dh@virginia.edu>
2015-01-18 17:44:32 -0500

    added sausage resposne

---
+++ responses/sausage.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*
+from AbstractResponse import AbstractResponse
+
+
+class ResponseSausage(AbstractResponse):
+
+    OVERRIDE_PRIORITY = 4
+
+    def __init__(self, msg, sender):
+        super(ResponseSausage, self).__init__(msg, sender)
+
+    def respond(self):
+        return "But, {}, you don't even have any money".format(self.sender)
+
+    @classmethod
+    def is_relevant_msg(cls, msg, sender):
+        return sender in ["Armadilldo", "Jonny G"] and 'sausage' in msg
+
