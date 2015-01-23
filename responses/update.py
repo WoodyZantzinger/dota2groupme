9507f4617020dee313eae3c221d103b20e6002a0
Woody Zantzinger <wz2ae@virginia.edu>
2015-01-22 22:16:07 -0500

    Update is now for one user

--- responses/update.py
+++ responses/update.py
@@ -3,14 +3,17 @@ from AbstractResponse import *
 from dota2py import api
 from dota2py import data
 
-class ResponseLast(AbstractResponse):
+class Update(AbstractResponse):
 
     RESPONSE_KEY = "#update"
 
-    HELP_RESPONSE = "Update everyone's last games"
+    message = "#update 1111111"
+
+    HELP_RESPONSE = "Update ones's last games"
 
     def __init__(self, msg, sender):
-        super(ResponseLast, self).__init__(msg, sender)
+        Update.message = msg
+        super(Update, self).__init__(msg, sender)
 
     def respond(self):
 
@@ -20,89 +23,90 @@ class ResponseLast(AbstractResponse):
 
         api.set_api_key(AbstractResponse.key)
 
-        #For every user
-        for name, account_id in AbstractResponse.GroupMetoDOTA.items():
-            print "Starting: {0}".format(name)
-            # Get a list of recent matches for the player
-            matches = api.get_match_history(account_id=account_id)["result"]["matches"]
+        account_id = int(Update.message.split()[1])
+        name = AbstractResponse.dotaID_to_name(account_id)
 
-            #Go through every match
-            for match in matches:
+        print "Starting: {0}".format(name)
+        # Get a list of recent matches for the player
+        matches = api.get_match_history(account_id=account_id)["result"]["matches"]
 
-                single_match = api.get_match_details(match["match_id"])["result"]
-                print "Checking: " + str(single_match["match_id"])
+        #Go through every match
+        for match in matches:
 
-                if (not AbstractResponse.has_dotaMatch(single_match["match_id"])):
-                    print "\t Adding: " + str(single_match["match_id"])
-                    AbstractResponse.add_dotaMatch(single_match)
-                    total_added += 1
+            single_match = api.get_match_details(match["match_id"])["result"]
+            print "Checking: " + str(single_match["match_id"])
 
-                    #Did this match set any new records# ?
-                    for player in single_match["players"]:
-                        #Is this a player we are tracking?
-                        if AbstractResponse.has_dotaID_num(int(player["account_id"])):
-                            #Yes! Check if they broke a record
-                            old_record = AbstractResponse.get_record(player["hero_id"])
+            if (not AbstractResponse.has_dotaMatch(single_match["match_id"])):
+                print "\t Adding: " + str(single_match["match_id"])
+                AbstractResponse.add_dotaMatch(single_match)
+                total_added += 1
 
-                            hero_name = data.get_hero_name(player["hero_id"])["localized_name"]
-                            player_name = AbstractResponse.dotaID_to_name(int(player["account_id"]))
+                #Did this match set any new records# ?
+                for player in single_match["players"]:
+                    #Is this a player we are tracking?
+                    if AbstractResponse.has_dotaID_num(int(player["account_id"])):
+                        #Yes! Check if they broke a record
+                        old_record = AbstractResponse.get_record(player["hero_id"])
 
-                            if old_record is None:
+                        hero_name = data.get_hero_name(player["hero_id"])["localized_name"]
+                        player_name = AbstractResponse.dotaID_to_name(int(player["account_id"]))
 
-                                #this is auto a new record!
-                                new_record = {"hero_id": player["hero_id"],
-                                              "max_kills": player["kills"],
-                                              "max_kills_player": player["account_id"],
-                                              "max_deaths": player["deaths"],
-                                              "max_deaths_player": player["account_id"],
-                                              "max_GPM": player["gold_per_min"],
-                                              "max_GPM_player": player["account_id"],
-                                              "min_GPM": player["gold_per_min"],
-                                              "min_GPM_player": player["account_id"],
-                                              "max_XPM": player["xp_per_min"],
-                                              "max_XPM_player": player["account_id"],
-                                              "min_XPM": player["xp_per_min"],
-                                              "min_XPM_player": player["account_id"],
-                                              }
-                                AbstractResponse.set_record(new_record)
+                        if old_record is None:
 
-                            else:
-                                #There is an existing record.. Lets see if this match was a game changer
-                                new_record = old_record.copy()
+                            #this is auto a new record!
+                            new_record = {"hero_id": player["hero_id"],
+                                          "max_kills": player["kills"],
+                                          "max_kills_player": player["account_id"],
+                                          "max_deaths": player["deaths"],
+                                          "max_deaths_player": player["account_id"],
+                                          "max_GPM": player["gold_per_min"],
+                                          "max_GPM_player": player["account_id"],
+                                          "min_GPM": player["gold_per_min"],
+                                          "min_GPM_player": player["account_id"],
+                                          "max_XPM": player["xp_per_min"],
+                                          "max_XPM_player": player["account_id"],
+                                          "min_XPM": player["xp_per_min"],
+                                          "min_XPM_player": player["account_id"],
+                                          }
+                            AbstractResponse.set_record(new_record)
 
-                                if old_record["max_kills"] < player["kills"]:
-                                    print "{0} just got {1} kills with {2}, a new record!".format(player_name, player["kills"], hero_name )
-                                    new_record["max_kills"] = player["kills"]
-                                    new_record["max_kills_player"] = player["account_id"]
+                        else:
+                            #There is an existing record.. Lets see if this match was a game changer
+                            new_record = old_record.copy()
 
-                                if old_record["max_deaths"] < player["deaths"]:
-                                    print "{0} just got {1} deaths with {2}, a new low!".format(player_name, player["deaths"], hero_name )
-                                    new_record["max_deaths"] = player["deaths"]
-                                    new_record["max_deaths_player"] = player["account_id"]
+                            if old_record["max_kills"] < player["kills"]:
+                                print "{0} just got {1} kills with {2}, a new record!".format(player_name, player["kills"], hero_name )
+                                new_record["max_kills"] = player["kills"]
+                                new_record["max_kills_player"] = player["account_id"]
 
-                                if old_record["max_GPM"] < player["gold_per_min"]:
-                                    print "{0} just got {1} GPM with {2}, a new record!".format(player_name, player["gold_per_min"], hero_name )
-                                    new_record["max_GPM"] = player["gold_per_min"]
-                                    new_record["max_GPM_player"] = player["account_id"]
+                            if old_record["max_deaths"] < player["deaths"]:
+                                print "{0} just got {1} deaths with {2}, a new low!".format(player_name, player["deaths"], hero_name )
+                                new_record["max_deaths"] = player["deaths"]
+                                new_record["max_deaths_player"] = player["account_id"]
 
-                                if old_record["min_GPM"] > player["gold_per_min"]:
-                                    print "{0} just got {1} GPM with {2}, a new low!".format(player_name, player["gold_per_min"], hero_name )
-                                    new_record["min_GPM"] = player["gold_per_min"]
-                                    new_record["min_GPM_player"] = player["account_id"]
+                            if old_record["max_GPM"] < player["gold_per_min"]:
+                                print "{0} just got {1} GPM with {2}, a new record!".format(player_name, player["gold_per_min"], hero_name )
+                                new_record["max_GPM"] = player["gold_per_min"]
+                                new_record["max_GPM_player"] = player["account_id"]
 
-                                if old_record["max_XPM"] < player["xp_per_min"]:
-                                    print "{0} just got {1} XPM with {2}, a new record!".format(player_name, player["xp_per_min"], hero_name )
-                                    new_record["max_XPM"] = player["xp_per_min"]
-                                    new_record["max_XPM_player"] = player["account_id"]
+                            if old_record["min_GPM"] > player["gold_per_min"]:
+                                print "{0} just got {1} GPM with {2}, a new low!".format(player_name, player["gold_per_min"], hero_name )
+                                new_record["min_GPM"] = player["gold_per_min"]
+                                new_record["min_GPM_player"] = player["account_id"]
 
-                                if old_record["min_XPM"] > player["xp_per_min"]:
-                                    print "{0} just got {1} XPM with {2}, a new low!".format(player_name, player["xp_per_min"], hero_name )
-                                    new_record["min_XPM"] = player["xp_per_min"]
-                                    new_record["min_XPM_player"] = player["account_id"]
+                            if old_record["max_XPM"] < player["xp_per_min"]:
+                                print "{0} just got {1} XPM with {2}, a new record!".format(player_name, player["xp_per_min"], hero_name )
+                                new_record["max_XPM"] = player["xp_per_min"]
+                                new_record["max_XPM_player"] = player["account_id"]
 
-                                AbstractResponse.set_record(new_record)
+                            if old_record["min_XPM"] > player["xp_per_min"]:
+                                print "{0} just got {1} XPM with {2}, a new low!".format(player_name, player["xp_per_min"], hero_name )
+                                new_record["min_XPM"] = player["xp_per_min"]
+                                new_record["min_XPM_player"] = player["account_id"]
 
-                else:
-                    print "\t Was Duplicate"
+                            AbstractResponse.set_record(new_record)
+
+            else:
+                print "\t Was Duplicate"
         final_string = "Updated {0} Matches".format(total_added)
         return final_string
