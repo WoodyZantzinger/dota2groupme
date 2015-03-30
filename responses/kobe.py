769f1beea2d95a5175c3dddeb18ee9585d6d0504
Michael Desrosiers <mjd2dh@virginia.edu>
2015-03-30 11:49:27 -0400

    added specific response functionality to #kobe

--- responses/kobe.py
+++ responses/kobe.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*
 from AbstractResponse import *
-import requests
 from random import randint
 
 class kobe(AbstractResponse):
@@ -8,6 +7,10 @@ class kobe(AbstractResponse):
     message = "#kobe"
 
     RESPONSE_KEY = "#kobe"
+
+    NAMES_1 = ["dick", "penis"]
+
+    NAMES_10 = ["liz", "erika", "paulina"]
 
     kobe_url = [
         "http://i.imgur.com/thhgY.gif",
@@ -19,14 +22,19 @@ class kobe(AbstractResponse):
         "http://i.imgur.com/G3b53.gif",
         "http://i.imgur.com/7cB7V.gif",
         "http://i.imgur.com/uUpX3.gif",
-        "http://i.imgur.com/eTCwx.gif"
+        "http://i.imgur.com/eTCwx.gif"  # 10
     ]
 
     def __init__(self, msg, sender):
-        kobe.message = msg
         super(kobe, self).__init__(msg, sender)
 
     def respond(self):
-        return kobe.kobe_url[randint(0,9)]
+        names10_in_msg = [name for name in kobe.NAMES_10 if name in self.msg.lower()]
+        if len(names10_in_msg):
+            return kobe.kobe_url[-1]
+        names1_in_msg = [name for name in kobe.NAMES_1 if name in self.msg.lower()]
+        if len(names1_in_msg):
+            return kobe.kobe_url[0]
+        return kobe.kobe_url[randint(0, 9)]
 
 
