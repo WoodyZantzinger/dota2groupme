da94748937f2923d032a0b485a7c0017267d6965
Michael Desrosiers <mjd2dh@virginia.edu>
2015-04-09 11:07:38 -0400

    added more specific gif responses

--- responses/specificgif.py
+++ responses/specificgif.py
@@ -8,7 +8,12 @@ class ResponseSpecificGif(AbstractResponse):
                 "neat" : "http://i.imgur.com/zCBQP8s.gif",
                 "imaprick" : "http://media.giphy.com/media/ueUKby1TWZPpe/giphy.gif",
                  "iwanttogo": "http://media.giphy.com/media/xMFawXGPvt05y/giphy.gif",
-                 "wheresthepizza": "http://media.giphy.com/media/qwvi00BCm6OmQ/giphy.gif"
+                 "wheresthepizza": "http://media.giphy.com/media/qwvi00BCm6OmQ/giphy.gif",
+                 "rememberme": "http://mrwgifs.com/wp-content/uploads/2013/08/Remember-Me-Bender-The-Great-Pharaoh-On-Futurama.gif",
+                 "haters": "http://i.imgur.com/75dta.gif",
+                 "datass": "http://26.media.tumblr.com/tumblr_lw7lvvZz9n1r5zq6ao1_500.gif",
+                 "aliens": "http://i0.kym-cdn.com/photos/images/original/000/155/594/yesitis2.gif?1311943181",
+                 "pooping": "http://media.tumblr.com/tumblr_m0t0ykVDMu1qzozj1.gif",
                  }
 
     def __init__(self, msg, sender):
