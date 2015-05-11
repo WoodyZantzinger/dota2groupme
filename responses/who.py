44ae11c4f8e68c2d4a0ca268e157f869d8840ae0
Michael Desrosiers <mjd2dh@virginia.edu>
2015-05-11 11:57:54 -0400

    added cooldown to #who

--- responses/who.py
+++ responses/who.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*
-from AbstractResponse import AbstractResponse
+from AbstractResponse import *
+from CooldownResponse import *
 import random
 
 
-class ResponseWho(AbstractResponse):
+class ResponseWho(ResponseCooldown):
 
     RESPONSE_KEY = "#who"
 
+    COOLDOWN = 1 * 60 * 60 / 4
+
     def __init__(self, msg, sender):
-        super(ResponseWho, self).__init__(msg, sender)
+        super(ResponseWho, self).__init__(msg, sender, self.__module__, ResponseWho.COOLDOWN)
 
     def respond(self):
-        people = []
-        for person, steamid in AbstractResponse.GroupMetoSteam.iteritems():
-            people.append(person)
-        return random.choice(people)
-
+        if self.is_sender_off_cooldown():
+            people = []
+            for person, steamid in AbstractResponse.GroupMetoSteam.iteritems():
+                people.append(person)
+            if "two thumbs" in self.msg:
+                return "^^ this guy"
+            return random.choice(people)
+        else:
+            print("not responding to #who because sender {} is on cooldown".format(self.sender))
