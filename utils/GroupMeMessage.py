174bf7240aca1cddaf9b71ab87791f5e6c8915c0
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2015-07-16 21:41:05 -0400

    Fixing Emoji Bug

--- utils/GroupMeMessage.py
+++ utils/GroupMeMessage.py
@@ -3,32 +3,31 @@ emojis = {
     "dino": [1, 62]
 }
 
-def return_emoticons(text, start):
-    first = text.find(":", start + 1)
-    if first > 0:
-        second = text.find(":", first + 1)
-        if second > 0:
-            return [text[first+1:second]], return_emoticons(text, second)
-    return None
+def return_emoticons(text):
+    return_val = []
+    for emoji in emojis:
+        first = text.find(emoji, 0)
+        if first > -1:
+            return_val.append(emoji)
+
+    return return_val
 
 def parse_message(text):
     values = {
           'bot_id' : 'f906f09e88ff3764c3c8b8c043',
     }
-    emoticons = return_emoticons(text, 0)
-    if emoticons is not None:
-        emoticons = filter(None, emoticons)
-        if len(emoticons) > 0:
-            char_map = []
-            for emoticon in emoticons:
-                if emoticon[0] in emojis:
-                    char_map.append(emojis[emoticon[0]])
-                    text = text.replace(":" + emoticon[0] + ":",u'\ufffd',1)
-            values["attachments"] = [{
-                'type' : 'emoji',
-                'charmap' : char_map,
-                'placeholder' : u'\ufffd'
-                                     }]
+    emoticons = return_emoticons(text)
+    if len(emoticons) > 0:
+        char_map = []
+        for emoticon in emoticons:
+            if emoticon in emojis:
+                char_map.append(emojis[emoticon])
+                text = text.replace(":" + emoticon + ":",u'\ufffd',1)
+        values["attachments"] = [{
+            'type' : 'emoji',
+            'charmap' : char_map,
+            'placeholder' : u'\ufffd'
+                                 }]
 
     values["text"] = text
     return values
