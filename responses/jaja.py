8c563f2caed61cc8b35c09a7c07f7b72a0fe2fed
Michael Desrosiers <mjd2dh@virginia.edu>
2015-07-30 15:59:52 -0400

    added #siono and jajajaja responses

---
+++ responses/jaja.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*
+
+from CooldownResponse import *
+import random
+
+
+class ResponseJaJa(ResponseCooldown):
+
+    COOLDOWN = 1 * 60 * 60 / 4
+
+    def __init__(self, msg):
+        super(ResponseJaJa, self).__init__(msg, self.__module__, ResponseJaJa.COOLDOWN)
+
+    def respond(self):
+        if self.is_sender_off_cooldown():
+            out = "ja" * random.randint(4, 8)
+            self.note_response(out)
+            return out
+        print("not responding to jaja because sender {} is on cooldown".format(self.sender))
+
+        return "Get a job"
+
+    @classmethod
+    def is_relevant_msg(cls, msg):
+        return "jaja" in msg.text.lower()
