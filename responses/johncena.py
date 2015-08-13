eea8b869d84ef00a7d2fb0049d24a7e6b83599ff
Michael Desrosiers <mjd2dh@virginia.edu>
2015-08-13 11:41:16 -0400

    added johncena.py

---
+++ responses/johncena.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*
+from AbstractResponse import *
+
+
+class ResponseJohnCena(AbstractResponse):
+
+    RESPONSE_KEY = "john cena"
+
+    def __init__(self, msg):
+        super(ResponseJohnCena, self).__init__(msg)
+
+    def respond(self):
+        return "DAAH DAAHT DOOT DAAH"
+
