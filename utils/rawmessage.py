e1b0b378bc9595121ae9be42c78b2b5091de97be
Michael Desrosiers <mjd2dh@virginia.edu>
2015-08-21 16:13:03 -0400

    added :emoji: replacement for messages as they come in

--- utils/rawmessage.py
+++ utils/rawmessage.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import GroupMeMessage
 
 
 class RawMessage(object):
@@ -19,6 +19,48 @@ class RawMessage(object):
         self.system = self.rawjson['system']
         self.text = self.rawjson['text']
         self.user_id = self.rawjson['user_id']
+        self.replace_emojis()
+
+    def replace_emojis(self):
+        #note the escape character for groupme emojis
+        esc = u'\ufffd'
+        #split the message on the escape character -- each item needs an :emoji: put between them
+        splittext = self.text.split(esc)
+
+        #allocate a varaible for the replacement mappings
+        replacemap = None
+
+        for attachment in self.attachments:
+            if attachment['type'] == u'emoji':
+                replacemap = attachment['charmap']
+
+        #if there isn't one, just bail - nothing to replace
+        if not replacemap:
+            return
+
+        # if for some reason the number of attachments doesn't match the number of $esc's, bail
+        if self.text.count(esc) != len(replacemap):
+            print("escape characters does not matches replacemap length!")
+            print("not modifying characters in message")
+            return
+
+        # reverse lookup the :emoji: texts from the [1, 61]-like attachment mappings
+        replacementtext = []
+        for replace in replacemap:
+            for emoji in GroupMeMessage.emojis:
+                if GroupMeMessage.emojis[emoji] == replace:
+                    replacementtext.append(emoji)
+
+        # if something failed to look up, just bail
+        if len(replacementtext) != len(replacemap):
+            return
+
+        out = []
+        for i, chunk in enumerate(splittext):
+            out.append(chunk)
+            if (i != (len(splittext) - 1)):
+                out.append(":" + replacementtext[i] + ":")
+        self.text = "".join(out)
 
 
 
