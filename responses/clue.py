4e6a6dd860f07e3279eaa214011e6dc60bf42d24
Stu Steen <ssteen3@gmail.com>
2015-10-28 20:19:47 -0500

    Clue Response
    Add clue response.
    This is a concatenation of who, what, and a new where response.
    I also added a places text file which serves as the library for the
    where command

---
+++ responses/clue.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*
+from AbstractResponse import *
+from CooldownResponse import *
+import random
+
+things = []
+with open(os.path.join("utils", "things.txt")) as f:
+    things = [line.rstrip('\n') for line in f]
+    
+places = []
+with open(os.path.join("utils", "places.txt")) as f:
+    places = [line.rstrip('\n') for line in f]
+
+people = []
+for person, steamid in AbstractResponse.GroupMetoSteam.iteritems():
+    people.append(person)
+
+class ResponseClue(ResponseCooldown):
+
+    RESPONSE_KEY = "#clue"
+
+    COOLDOWN = 1 * 60 * 60 / 4
+
+    def __init__(self, msg):
+        super(ResponseClue, self).__init__(msg, self.__module__, ResponseClue.COOLDOWN)
+
+    def respond(self):
+        if self.is_sender_off_cooldown():
+            outwho = random.choice(people)
+            outwhat = random.choice(things)
+            outwhere = random.choice(places)
+            out = "%s with the %s in the %s." % (outwho, outwhat, outwhere)
+            self.note_response(out)
+            return out
+        else:
+            print("not responding to #clue because sender {} is on cooldown".format(self.msg.name))
+            
+            
+    
