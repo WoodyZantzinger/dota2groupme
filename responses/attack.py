839c5c3fe50a724d6c32166bce6771fb1b65c7e2
Stu Steen <ssteen3@gmail.com>
2015-11-03 14:17:51 -0600

    pokemon

---
+++ responses/attack.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*
+from AbstractResponse import *
+from CooldownResponse import *
+import random
+
+moves = []
+with open(os.path.join("utils", "moves.txt")) as f:
+    moves = [line.rstrip('\n') for line in f]
+
+
+class ResponseAttack(ResponseCooldown):
+
+    RESPONSE_KEY = "#attack"
+
+    COOLDOWN = 1 * 60 * 60 / 4
+
+    def __init__(self, msg):
+        super(ResponseAttack, self).__init__(msg, self.__module__, ResponseAttack.COOLDOWN)
+
+    def respond(self):
+        if self.is_sender_off_cooldown():
+            out = random.choice(moves)
+            self.note_response(out)
+            return out
+        else:
+            print("not responding to #attack because sender {} is on cooldown".format(self.msg.name))
