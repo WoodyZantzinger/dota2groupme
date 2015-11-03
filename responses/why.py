43756c5478300ece9e6f7e16a367b1cd18540f38
Stu Steen <ssteen3@gmail.com>
2015-11-03 13:36:44 -0600

    added why command

---
+++ responses/why.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*
+from AbstractResponse import *
+from CooldownResponse import *
+import random
+
+reasons = []
+with open(os.path.join("utils", "reasons.txt")) as f:
+    things = [line.rstrip('\n') for line in f]
+
+
+class ResponseWhy(ResponseCooldown):
+
+    RESPONSE_KEY = "#why"
+
+    COOLDOWN = 1 * 60 * 60 / 4
+
+    def __init__(self, msg):
+        super(ResponseWhy, self).__init__(msg, self.__module__, ResponseWhy.COOLDOWN)
+
+    def respond(self):
+        if self.is_sender_off_cooldown():
+            out = random.choice(reasons)
+            self.note_response(out)
+            return out
+        else:
+            print("not responding to #why because sender {} is on cooldown".format(self.msg.name))
