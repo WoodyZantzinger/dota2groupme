62da5c9075459b0fcb92b0e86aab680c42ddeb2f
mjdesrosiers <mjd2dh@virginia.edu>
2016-03-14 18:13:58 -0400

    improved kobe's response algorithm

--- responses/kobe.py
+++ responses/kobe.py
@@ -14,16 +14,16 @@ class kobe(AbstractResponse):
     NAMES_10 = ["liz", "erika", "paulina"]
 
     kobe_url = [
-        "http://i.imgur.com/thhgY.gif",
-        "http://i.imgur.com/dECdK.gif",
-        "http://i.imgur.com/hr8r3.gif",
-        "http://i.imgur.com/Sv9tv.gif",
-        "http://i.imgur.com/qIJB5.gif",
-        "http://i.imgur.com/QYVa6.gif",
+        #"http://i.imgur.com/thhgY.gif",
+        #"http://i.imgur.com/dECdK.gif",
+        #"http://i.imgur.com/hr8r3.gif",
+        #"http://i.imgur.com/Sv9tv.gif",
+        #"http://i.imgur.com/qIJB5.gif",
+        #"http://i.imgur.com/QYVa6.gif",
         "http://i.imgur.com/G3b53.gif",
-        "http://i.imgur.com/7cB7V.gif",
-        "http://i.imgur.com/uUpX3.gif",
-        "http://i.imgur.com/eTCwx.gif"  # 10
+        #"http://i.imgur.com/7cB7V.gif",
+        #"http://i.imgur.com/uUpX3.gif",
+        #"http://i.imgur.com/eTCwx.gif"  # 10
     ]
 
     def __init__(self, msg):
@@ -36,6 +36,6 @@ class kobe(AbstractResponse):
         names1_in_msg = [name for name in kobe.NAMES_1 if name in self.msg.text.lower()]
         if len(names1_in_msg):
             return kobe.kobe_url[0]
-        return kobe.kobe_url[randint(0, 9)]
+        return kobe.kobe_url[randint(0, len(kobe.kobe_url) - 1)]
 
 
