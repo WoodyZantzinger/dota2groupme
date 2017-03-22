783608bcb2e3bf4ac2826b1b0bacf27335efb1eb
mjdesrosiers <mjd2dh@virginia.edu>
2017-03-22 18:28:06 -0400

    added statistic for number of messages sent in a day

---
+++ statistics/StatisticMessagesSent.py
@@ -0,0 +1,17 @@
+
+from AbstractStatistics import *
+
+class StatisticMessagesSent(AbstractStatistics):
+
+    ENABLED = True
+
+    def __init__(self, messages, mod=None):
+        super(StatisticMostMessagesSent, self).__init__(messages)
+        self.messages = messages
+
+    def respond(self):
+        n = len(self.messages)
+        MSG_FORMAT = "Total number of messages sent: {n}"
+        return MSG_FORMAT.format(n=n)
+
+
