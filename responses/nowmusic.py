1d43d3dcc223c8a2f87f334ebad21cbed62a0688
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2017-04-19 13:42:22 -0400

    more Spotify permissions

--- responses/nowmusic.py
+++ responses/nowmusic.py
@@ -15,7 +15,7 @@ class nowmusic(AbstractResponse):
 
     COOLDOWN = 1 * 60 * 60 * 3 / 2
 
-    url = "https://api.spotify.com/v1/me/player/currently-playing?Authorization={token}"
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
 
     def __init__(self, msg):
         super(nowmusic, self).__init__(msg)
@@ -44,6 +44,7 @@ class nowmusic(AbstractResponse):
             "client_id=f8597c3f9afb4c1f9f0d3e8d5b53d4ae"
             "&response_type=code"
             "&redirect_uri=https://young-fortress-3393.herokuapp.com/spotify_callback"
+            "&scope=user-read-currently-playing"
             "&state=" + self.msg.sender_id
             )
             return URL
