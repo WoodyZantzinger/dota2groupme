b5467528bfea5925b850bcd4f0662cd9b763ce09
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2017-04-21 10:10:24 -0400

    reauth for Spotify

--- responses/nowmusic.py
+++ responses/nowmusic.py
@@ -25,17 +25,38 @@ class nowmusic(AbstractResponse):
         SpotifyUsers = conn.dota2bot.spotify
         temp = SpotifyUsers.find_one({'GroupmeID': self.msg.sender_id})
         if temp is not None:
-            Token = temp["access_token"]
-            headers = {'Authorization': 'Bearer ' + str(Token)}
+            headers = {'Authorization': 'Bearer ' + str(temp["access_token"])}
             try:
                 response = requests.get(nowmusic.url, headers=headers)
-                currently_playing = response.json()["is_playing"]
-                song = response.json()["item"]["name"]
-                artist = response.json()["item"]["artists"][0]["name"]
-                if currently_playing:
-                    out = "Currently listening to " + song + " by " + artist
+                if (response.status_code == 401):
+                    r = requests.post('https://accounts.spotify.com/api/token', data = {
+                        'grant_type':'authorization_code',
+                        'client_id':'f8597c3f9afb4c1f9f0d3e8d5b53d4ae',
+                        'redirect_uri':'https://young-fortress-3393.herokuapp.com/spotify_callback',
+                        'client_secret': oAuth_util.get_spotify_key(),
+                        'code': temp["refresh_token"]})
+
+
+                    SpotifyData = r.json()
+                    temp["access_token"] = SpotifyData["access_token"]
+                    temp["refresh_token"] = SpotifyData["refresh_token"]
+
+                    SpotifyUsers.update({'_id': temp["_id"]}, {"$set": temp}, upsert=True)
+
+                    #Try again
+                    headers = {'Authorization': 'Bearer ' + str(temp["access_token"])}
+                    response = requests.get(nowmusic.url, headers=headers)
+                elif (response.status_code == 200):
+                    currently_playing = response.json()["is_playing"]
+                    song = response.json()["item"]["name"]
+                    artist = response.json()["item"]["artists"][0]["name"]
+                    if currently_playing:
+                        out = "Currently listening to " + song + " by " + artist
+                    else:
+                        out = "Last listened to " + song + " by " + artist
                 else:
-                    out = "Last listened to " + song + " by " + artist
+                    out = "Something went wrong: HTTP status != 200 or 401 (blame Mike)"
+
             except Exception as e:
                 out = "Something went wrong: " + str(e)
             return out
