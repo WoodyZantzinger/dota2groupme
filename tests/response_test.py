1fc42f12da16683c36c572dfe16b715e4a0be6eb
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2017-04-27 16:13:53 -0400

    initial test frame

---
+++ tests/response_test.py
@@ -0,0 +1,32 @@
+__author__ = 'woodyzantzinger'
+
+import unittest
+from .. import bot
+
+msg = """{
+  "attachments": [],
+  "avatar_url": "http://i.groupme.com/123456789",
+  "created_at": 1302623328,
+  "group_id": "16905359",
+  "id": "1",
+  "name": "Woody",
+  "sender_id": "7340477",
+  "sender_type": "user",
+  "source_guid": "GUID",
+  "system": false,
+  "text": "#last",
+  "user_id": "73450477"
+    } """
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        bot.app.config['TESTING'] = True
+        self.app = bot.app.test_client()
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'Hello world!' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_emulate.py
@@ -0,0 +1,5 @@
+from utils.emulate.emulate import *
+
+while (1):
+    x = input('Input Sentence: ')
+    print generate_response(x, 0)
