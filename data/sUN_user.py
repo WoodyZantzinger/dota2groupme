9054f84f6a0067344bc64c10c8109ed29f2ced74
mjdesrosiers <mjd2dh@virginia.edu>
2021-02-06 18:06:26 -0500

    Added __getitem__ and __setitem__ functionality to sUN_user class to ease use

--- data/sUN_user.py
+++ data/sUN_user.py
@@ -1,13 +1,21 @@
 
 class sUN_user():
-     def __init__(self, kvdict):
+    def __init__(self, kvdict):
         self.values = kvdict
 
-     def make_db_object(self):
-         return self.values
+    def make_db_object(self):
+        return self.values
 
-     def __str__(self):
-         out = ""
-         for part in self.values:
-             out = out + f"{part} -> {self.values[part]}\n"
-         return out
+    def __getitem__(self, key):
+        if not key in self.values:
+            return None
+        return self.values[key]
+
+    def __setitem__(self,  key, value):
+        self.values[key] = value
+
+    def __str__(self):
+        out = ""
+        for part in self.values:
+            out = out + f"{part} -> {self.values[part]}\n"
+        return out
