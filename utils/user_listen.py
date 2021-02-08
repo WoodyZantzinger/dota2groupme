2f029695867c3c63813cfb756222df8f1024ecc3
Woody <wz2ae@virginia.edu>
2021-02-08 18:10:34 -0500

    small typo

--- utils/user_listen.py
+++ utils/user_listen.py
@@ -31,7 +31,7 @@ def getNew(c_ID, numCalls):
             if message_type == "like.create": message_type_token = "Like"
             if message_type == "ping": message_type_token = "Ping"
 
-            if message_type_token != "Ping":
+            if message_type != "ping":
                 message_data = single_message["data"]["subject"]
                 print(message_data["text"])
                 message_type_token = "Message"
