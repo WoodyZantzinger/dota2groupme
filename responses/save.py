f43b5207121956d8972e36c57fe904f68eee25d1
mjdesrosiers <mjd2dh@virginia.edu>
2021-02-21 19:54:58 -0500

    Adding #save functionality for quoted messages (50/50 on working vs not working). Goes to groupmemes/sUN-uploads

---
+++ responses/save.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*
+
+from .CooldownResponse import *
+import random
+from utils import get_groupme_messages
+from utils import GroupMeMessage
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive# Rename the downloaded JSON file to client_secrets.json
+import urllib.request
+
+GROUPMEME_FOLDER = "groupmemes"
+SUN_UPLOADS_FOLDER = "sUN_uploads"
+
+class ResponseSave(ResponseCooldown):
+    COOLDOWN = -1
+    RESPONSE_KEY = "#save"
+
+    def __init__(self, msg):
+        super(ResponseSave, self).__init__(msg, self, ResponseSave.COOLDOWN)
+
+
+    def get_referenced_image_urls(self):
+        if not hasattr(self.msg, "attachments"):
+            return
+
+        attachments = self.msg.attachments
+
+        found_quoted_message = False
+        for attachment in attachments:
+            if "reply_id" in attachment and attachment['type'] == "reply":
+                found_quoted_message = True
+                break
+
+        if not found_quoted_message:
+            return
+
+        reply_id = attachment['reply_id']
+        group_id = self.msg.group_id
+
+        msg = get_groupme_messages.get_exact_group_message(group_id, reply_id)
+        msg = msg['response']['message']
+        image_attachments = []
+        if "attachments" in msg:
+            for attachment in msg['attachments']:
+                if attachment['type'] == "image":
+                    image_attachments.append(attachment)
+        return image_attachments
+
+    def upload_files_to_pydrive(self, local_fnames):
+        secrets_data = self.get_response_storage("client_secrets")
+        secrets_data = secrets_data[1:-1]
+        with open("client_secrets.json", "w+") as f:
+            f.write(secrets_data)
+        pass
+        gauth = GoogleAuth()
+        # Try to load saved client credentials
+        gauth.LoadCredentialsFile("mycreds.txt")
+        if gauth.credentials is None:
+            # Authenticate if they're not there
+            gauth.LocalWebserverAuth()
+        elif gauth.access_token_expired:
+            # Refresh them if expired
+            gauth.Refresh()
+        else:
+            # Initialize the saved creds
+            gauth.Authorize()
+        # Save the current credentials to a file
+        gauth.SaveCredentialsFile("mycreds.txt")
+
+        drive = GoogleDrive(gauth)  # List files in Google Drive
+        #fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+        f = drive.ListFile({"q": "mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+
+        fID = None
+        for folder in f:
+            if folder['title'] == SUN_UPLOADS_FOLDER:
+                fID = folder['id']
+
+        if not fID:
+            return
+
+        for fname in local_fnames:
+            file = drive.CreateFile({'parents': [{'id': fID}]})
+            file.SetContentFile(fname)
+            file.Upload()
+
+
+    def _respond(self):
+        image_attachments = self.get_referenced_image_urls()
+        filenames = self.save_images_to_local(image_attachments)
+        self.upload_files_to_pydrive(filenames)
+
+    def save_images_to_local(self, image_attachments):
+        local_fnames = []
+        for attachment in image_attachments:
+            uuid = attachment['url'].split('.')[-1]
+            fname = attachment['url'][:(-1 * (len(uuid)+1))]
+            ftype = fname.split('.')[-1]
+            save_fname = "".join([uuid, '.', ftype])
+            urllib.request.urlretrieve(attachment['url'],
+                                       save_fname)
+            local_fnames.append(save_fname)
+        return local_fnames
---
+++ utils/get_groupme_messages.py
@@ -0,0 +1,39 @@
+from data import DataAccess
+import requests
+import json
+
+EXACT_MESSAGE_ENDPOINT = "https://api.groupme.com/v3/groups/{group_id}/messages/{msg_id}"
+LIST_OF_MESSAGES_ENDPOINT = "https://api.groupme.com/v3/groups/{group_id}/messages"
+
+
+def get_exact_group_message(group_id, message_id):
+    url = EXACT_MESSAGE_ENDPOINT.format(
+        group_id=group_id,
+        msg_id=message_id
+    )
+    key = DataAccess.get_secrets()["GROUPME_AUTH"]
+
+    values = {
+        "token": key,
+        "limit": 1,
+    }
+
+    req = requests.get(url, params=values)
+    response = json.loads(req.text)
+    return response
+
+def get_list_of_messages_before(group_id, message_id, limit):
+    url = LIST_OF_MESSAGES_ENDPOINT.format(
+        group_id=group_id,
+    )
+    key = DataAccess.get_secrets()["GROUPME_AUTH"]
+
+    values = {
+        "before_id": message_id,
+        "token": key,
+        "limit": 1,
+    }
+
+    req = requests.get(url, params=values)
+    response = json.loads(req.text)
+    return response
