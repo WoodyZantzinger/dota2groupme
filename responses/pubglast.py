0778829e73029f7473a3b54136ad3a989517fbb5
mjdesrosiers <mjd2dh@virginia.edu>
2022-02-13 09:04:46 -0500

    enhanced #pubglast to also send the names of your squadmates

--- responses/pubglast.py
+++ responses/pubglast.py
@@ -15,7 +15,7 @@ class ResponsePUBGLast(AbstractResponse):
     def _respond(self):
         out = ""
 
-        template = "{name} did {damage} damage for {numKills} kills (placing {killRank} in kills) to finish {result} in a {gameType}\n"
+        template = "{name} did {damage} damage for {numKills} kills (placing {killRank} in kills) to finish {result} in a {gameType}. Squad:\n{squad}"
 
         user = DataAccess.DataAccess().get_user("GROUPME_ID", self.msg.sender_id)
         canonical_name = user['Name']
@@ -37,13 +37,15 @@ class ResponsePUBGLast(AbstractResponse):
         if my_roster:
             idxs = [i if (p.name == player.name) else -1 for i, p in enumerate(my_roster.participants)]
             me = my_roster.participants[max(idxs)]
+            squad_names = [p.name for p in my_roster.participants if p.name != player.name]
             print("found roster")
             out = template.format(name = me.name,
                                   damage = int(round(me.damage_dealt, 0)),
                                   numKills = me.kills,
                                   killRank = me.kill_place,
                                   result = me.win_place,
-                                  gameType = match.game_mode)
+                                  gameType = match.game_mode,
+                                  squad="\n".join(squad_names))
 
             #print(out)
             return out
