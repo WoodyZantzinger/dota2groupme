50be35abe220d9c731d99b37883b33cdf660087f
mjdesrosiers <mjd2dh@virginia.edu>
2022-10-06 13:29:05 -0400

    Test maybe fixed videos not downloading

--- utils/BaseMessage.py
+++ utils/BaseMessage.py
@@ -164,10 +164,66 @@ class TelegramMessage(BaseMessage):
     def get_quoted_message_id(self):
         return self.message['reply_to_message']['id']
 
+    @staticmethod
+    async def save_photos(obj):
+        attachments = obj.effective_message['reply_to_message']['effective_attachment']
+        attached_items = []
+        if type(attachments) == list:
+            file_ids = set([item['file_id'][0:15] for item in attachments])
+            for fid in file_ids:
+                photo_set = [item for item in attachments if item['file_id'][0:15] == fid]
+                image_sizes = [item['file_size'] for item in photo_set]
+                largest_item = [item for item in photo_set if item['file_size'] == max(image_sizes)][0]
+                ps = PhotoSize(
+                    largest_item['file_id'],
+                    largest_item['file_unique_id'],
+                    largest_item['width'],
+                    largest_item['height'],
+                    largest_item['file_size'],
+                )
+                attached_items.append(ps)
+        out_fnames = []
+        print(attached_items)
+        for item in attached_items:
+            save_name = ""
+            try:
+                save_name = item.file_name
+            except:
+                save_name = str(uuid.uuid4()) + ".png"
+            # @TODO add a bot object to the photothing initialization so it can save?
+            print(save_name)
+            f_obj = await obj.tg_bot.get_file(file_id=item.file_id)
+            fname = await f_obj.download()
+            print(fname)
+            out_fnames.append(fname.name)
+        return out_fnames
+
+    @staticmethod
+    async def save_video(obj):
+        pass
+
+    @staticmethod
+    async def save_audio(obj):
+        pass
+
     async def save_attachments_to_local(self):
         print("oh, hello there.")
         attachments = self.effective_message['reply_to_message']['effective_attachment']
         attached_items = []
+        # types = {
+        #     'photo': TelegramMessage.save_photos,
+        #     'video': TelegramMessage.save_video,
+        #     'video_note': TelegramMessage.save_video,
+        #     'voice': TelegramMessage.save_audio,
+        #     'audio': TelegramMessage.save_audio,
+        # }
+        #
+        # fnames = []
+        # for key in types:
+        #     if key in self.effective_message['reply_to_message']:
+        #         fnames.append(types[key](self))
+
+
         if type(attachments) == list:
             file_ids = set([item['file_id'][0:15] for item in attachments])
             for fid in file_ids:
@@ -187,7 +243,7 @@ class TelegramMessage(BaseMessage):
                 file_id=attachments['file_id'],
                 file_unique_id=attachments["file_unique_id"],
                 thumb=None,
-                file_name=attachments["file_name"],
+                # file_name=attachments["file_name"],
                 mime_type=attachments["mime_type"],
                 file_size=attachments["file_size"],
                 bot=self.tg_bot
@@ -197,13 +253,13 @@ class TelegramMessage(BaseMessage):
         out_fnames = []
         print(attached_items)
         for item in attached_items:
-            save_name = ""
-            try:
-                save_name = item.file_name
-            except:
-                save_name = str(uuid.uuid4()) + ".png"
-            # @TODO add a bot object to the photothing initialization so it can save?
-            print(save_name)
+            # save_name = ""
+            # try:
+            #     save_name = item.file_name
+            # except:
+            #     save_name = str(uuid.uuid4()) + ".png"
+            # # @TODO add a bot object to the photothing initialization so it can save?
+            # print(save_name)
             obj = await self.tg_bot.get_file(file_id=item.file_id)
             fname = await obj.download()
             print(fname)
