c3148d784f820a2e1456b4c84e7901981b7fcba1
Woody <wz2ae@virginia.edu>
2023-03-12 17:25:50 -0400

    Chat GPT

---
+++ responses/chatgpt.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*
+
+from .CooldownResponse import *
+import openai
+
+
+class ResponseChat(ResponseCooldown):
+
+    message = "#gpt"
+
+    RESPONSE_KEY = "#gpt"
+
+    COOLDOWN = 1 * 60 * 60 * 3 / 2
+
+    def __init__(self, msg):
+        super(ResponseChat, self).__init__(msg, self, ResponseChat.COOLDOWN)
+
+    def _respond(self):
+
+        openai.api_key = DataAccess.get_secrets()['OPENAI_KEY']
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": self.msg.text.partition(' ')[2]}
+            ]
+        )
+
+        return response['choices'][0]['message']['content']
